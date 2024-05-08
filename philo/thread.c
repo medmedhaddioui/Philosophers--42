c2baed64aad3072d1b309a4ba2e304c0bfe9a15e
Mohammed El Haddioui <mel-hadd@c4r6p10.1337.ma>
2024-05-08 15:07:08 +0100

    Update

--- philo/thread.c
+++ philo/thread.c
@@ -6,7 +6,7 @@
 /*   By: mel-hadd <mel-hadd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/05 16:28:53 by mel-hadd          #+#    #+#             */
-/*   Updated: 2024/05/07 16:04:41 by mel-hadd         ###   ########.fr       */
+/*   Updated: 2024/05/07 19:22:27 by mel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,29 @@
 void *routine (void *data)
 {
 	t_philo *philo = data;
-
-	if (philo->philo_id % 2 == 0)
+	while (1)
 	{
-		thinking(philo);	
+		if (philo->philo_id % 2 == 0)
+			thinking(philo);
+		pthread_mutex_lock(philo->first_fork);
+		pthread_mutex_lock(philo->second_fork);
+			eating(philo);
+			sleeping(philo);
+		pthread_mutex_unlock(philo->first_fork);
+		pthread_mutex_unlock(philo->second_fork);
 	}
-	pthread_mutex_lock(philo->first_fork);
-	pthread_mutex_lock(philo->second_fork);
-		eating(philo);
-		sleeping(philo);
-	pthread_mutex_unlock(philo->first_fork);
-	pthread_mutex_unlock(philo->second_fork);
 
 	return NULL;
 }
 
 void  thread_add(t_philo *philos, t_philo arg)
 {
+	
 	int i;
 	i = -1;
+	if (philos->nb_times_to_eat == 0)
+		return 1;
+	
 	while (++i < arg.nb_of_philos)
 		pthread_create(&philos[i].thread, NULL, &routine, (void *)&philos[i]);
 	i = 0;
