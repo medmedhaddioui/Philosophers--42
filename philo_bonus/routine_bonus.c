29714cbc645d4dad70763a6535f8148cec789927
Mohammed El Haddioui <mel-hadd@e2r4p1.1337.ma>
2024-05-28 14:43:19 +0100

    msg

--- philo_bonus/routine_bonus.c
+++ philo_bonus/routine_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mel-hadd <mel-hadd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 14:09:02 by mel-hadd          #+#    #+#             */
-/*   Updated: 2024/05/28 13:54:55 by mel-hadd         ###   ########.fr       */
+/*   Updated: 2024/05/28 14:42:59 by mel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,12 @@
 void	printf_info(char *s, t_philo *philo)
 {
 	int	*value_sem;
-
+	int d_flag;
+	sem_wait(philo->sem_flag);
+	d_flag = philo->dead_flag;
+	sem_post(philo->sem_flag);
 	value_sem = (int *)philo->write;
-	if (*value_sem == 0)
+	if (*value_sem == 0 && d_flag == 1)
 		return ;
 	sem_wait(philo->write);
 	philo->time = get_current_time_ms() - philo->start_time;
